a81cb7ba2c2ec28d0306bd9166c43534ab2244d4
soldiergit <soldier_wyyx@163.com>
2019-10-12 09:01:14 +0800

    修复BUG

--- src/views/modules/enterprise/apply/ent-recruitment.vue
+++ src/views/modules/enterprise/apply/ent-recruitment.vue
@@ -131,197 +131,197 @@
 import EntRecuitmentDetails from './ent-recruitment-details'
 
 export default {
-    data () {
-        return {
-            tempRec: null, // 招聘临时信息
-            consenttip: '您确认要通过该审核吗？该操作不可撤回！',
-            retreattip: '您确认不通过该审核吗？该操作不可撤回！',
-            shenhe: false,
-            consentVisible: false,
-            retreatVisible: false,
-            hasApply: '0',
-            dataForm: {
-                recruitmentInfoId: '',
-                recruitmentPost: '',
-                matchTime: new Date(),
-                idDel: 0
-            },
-            dataList: [],
-            pageIndex: 1,
-            pageSize: 10,
-            totalPage: 0,
-            dataListLoading: false,
-            dataListSelections: []
-        }
-    },
-    components: {
-        EntRecuitmentDetails
-    },
-    activated () {
-        this.getDataList()
-    },
-    methods: {
-        // 格式化区域显示
-        formatterZone (row, column, cellValue) {
-            return cellValue === '0' ? '是' : '否'
-        },
-        // 详情
-        detailHandle (id) {
-            console.log(id)
-            this.shenhe = true
-            this.$nextTick(() => {
-                this.$refs.details.init(id)
-            })
-        },
-        // 通过
-        consentHandle (item) {
-            this.consentVisible = true
-            this.tempRec = item
-        },
-        // 不通过
-        retreatHandle (item) {
-            this.retreatVisible = true
-            this.tempRec = item
-        },
-        // 获取数据列表
-        getDataList () {
-            this.dataListLoading = true
-            this.$http({
-                url: this.$http.adornUrl('/enterprise/recruitment/info/list'),
-                method: 'get',
-                params: this.$http.adornParams({
-                    'currPage': this.pageIndex,
-                    'pageSize': this.pageSize,
-                    'inApply': this.hasApply
-                })
-            }).then(({data}) => {
-                console.log(data)
-                if (data.code === 500) {
-                    this.$message.error(data.msg)
-                }
-                if (data && data.code === 0) {
-                    this.dataList = data.page.list
-                    this.totalPage = data.page.totalCount
-                } else {
-                    this.dataList = []
-                    this.totalPage = 0
-                }
-                this.dataListLoading = false
-            })
-        },
-        //  每页数
-        sizeChangeHandle (val) {
-            this.pageSize = val
-            this.pageIndex = 1
-            this.getDataList()
-        },
-        // 当前页
-        currentChangeHandle (val) {
-            this.pageIndex = val
-            this.getDataList()
-        },
-        // 多选
-        selectionChangeHandle (val) {
-            this.dataListSelections = val
-        },
-        // 审批
-        applyMatchHandle (id) {
-            this.$nextTick(() => {
-                // this.$refs.award.init(id)
-            })
-        },
-        // 审批通过
-        applyConsentHandle () {
-            this.$http({
-                url: this.$http.adornUrl('/enterprise/recruitment/info/recExamine'),
-                method: 'post',
-                params: this.$http.adornParams({
-                    'recruitmentInfoId': this.tempRec.recruitmentInfoId,
-                    'inApply': '1'
-                }, false)
-            }).then(({data}) => {
-                this.$message({
-                    type: 'success',
-                    message: '提交成功!'
-                })
-                this.retreatVisible = false
-                this.getDataList()
-            })
-        },
-        // 审批不通过
-        applyRetreatHandle () {
-            this.$http({
-                url: this.$http.adornUrl('/enterprise/recruitment/info/recExamine'),
-                method: 'post',
-                params: this.$http.adornParams({
-                    'recruitmentInfoId': this.tempRec.recruitmentInfoId,
-                    'inApply': '0'
-                }, false)
-            }).then(({data}) => {
-                this.$message({
-                    type: 'success',
-                    message: '提交成功!'
-                })
-                this.retreatVisible = false
-                this.getDataList()
-            })
-        },
-        isDelete (item) {
-            if ((item.projectMatchApplyStatus === 0) &&
-                this.isAuth('innovate:match:delete')) {
-                return true
-            }
-        },
-        // 删除
-        deleteHandle (id) {
-            var canDelete = true
-            var matchIds = id ? [id] : this.dataListSelections.map(item => {
-                if ((item.projectInfoEntity.projectMatchApplyStatus > 0) ||
-                    !this.isAuth('innovate:match:delete')) {
-                    canDelete = false
-                } else {
-                    canDelete = false
-                }
-                return item.projectInfoEntity.matchId
-            })
-            this.$confirm(`确定要进行[${id ? '删除' : '批量删除'}]操作?`, '提示', {
-                confirmButtonText: '确定',
-                cancelButtonText: '取消',
-                type: 'warning'
-            }).then(() => {
-                if (canDelete) {
-                    this.$http({
-                        url: this.$http.adornUrl('/innovate/match/info/delete'),
-                        method: 'post',
-                        data: this.$http.adornData(matchIds, false)
-                    }).then(({data}) => {
-                        if (data && data.code === 0) {
-                            this.$message({
-                                message: '操作成功',
-                                type: 'success',
-                                duration: 1500,
-                                onClose: () => {
-                                    this.getDataList()
-                                }
-                            })
-                        } else {
-                            this.$message.error(data.msg)
-                        }
-                    })
-                } else {
-                    this.$message({
-                        message: '包含不可删除项目',
-                        type: 'error',
-                        duration: 1500,
-                        onClose: () => {
-                            this.getDataList()
-                        }
-                    })
-                }
-            }).catch(() => {
-            })
-        }
+  data () {
+    return {
+      tempRec: null, // 招聘临时信息
+      consenttip: '您确认要通过该审核吗？该操作不可撤回！',
+      retreattip: '您确认不通过该审核吗？该操作不可撤回！',
+      shenhe: false,
+      consentVisible: false,
+      retreatVisible: false,
+      hasApply: '0',
+      dataForm: {
+        recruitmentInfoId: '',
+        recruitmentPost: '',
+        matchTime: new Date(),
+        idDel: 0
+      },
+      dataList: [],
+      pageIndex: 1,
+      pageSize: 10,
+      totalPage: 0,
+      dataListLoading: false,
+      dataListSelections: []
     }
+  },
+  components: {
+    EntRecuitmentDetails
+  },
+  activated () {
+    this.getDataList()
+  },
+  methods: {
+        // 格式化区域显示
+    formatterZone (row, column, cellValue) {
+      return cellValue === '0' ? '是' : '否'
+    },
+        // 详情
+    detailHandle (id) {
+      console.log(id)
+      this.shenhe = true
+      this.$nextTick(() => {
+        this.$refs.details.init(id)
+      })
+    },
+        // 通过
+    consentHandle (item) {
+      this.consentVisible = true
+      this.tempRec = item
+    },
+        // 不通过
+    retreatHandle (item) {
+      this.retreatVisible = true
+      this.tempRec = item
+    },
+        // 获取数据列表
+    getDataList () {
+      this.dataListLoading = true
+      this.$http({
+        url: this.$http.adornUrl('/enterprise/recruitment/info/list'),
+        method: 'get',
+        params: this.$http.adornParams({
+          'currPage': this.pageIndex,
+          'pageSize': this.pageSize,
+          'inApply': this.hasApply
+        })
+      }).then(({data}) => {
+        console.log(data)
+        if (data.code === 500) {
+          this.$message.error(data.msg)
+        }
+        if (data && data.code === 0) {
+          this.dataList = data.page.list
+          this.totalPage = data.page.totalCount
+        } else {
+          this.dataList = []
+          this.totalPage = 0
+        }
+        this.dataListLoading = false
+      })
+    },
+        //  每页数
+    sizeChangeHandle (val) {
+      this.pageSize = val
+      this.pageIndex = 1
+      this.getDataList()
+    },
+        // 当前页
+    currentChangeHandle (val) {
+      this.pageIndex = val
+      this.getDataList()
+    },
+        // 多选
+    selectionChangeHandle (val) {
+      this.dataListSelections = val
+    },
+        // 审批
+    applyMatchHandle (id) {
+      this.$nextTick(() => {
+                // this.$refs.award.init(id)
+      })
+    },
+        // 审批通过
+    applyConsentHandle () {
+      this.$http({
+        url: this.$http.adornUrl('/enterprise/recruitment/info/recExamine'),
+        method: 'post',
+        params: this.$http.adornParams({
+          'recruitmentInfoId': this.tempRec.recruitmentInfoId,
+          'inApply': '1'
+        }, false)
+      }).then(({data}) => {
+        this.$message({
+          type: 'success',
+          message: '提交成功!'
+        })
+        this.retreatVisible = false
+        this.getDataList()
+      })
+    },
+        // 审批不通过
+    applyRetreatHandle () {
+      this.$http({
+        url: this.$http.adornUrl('/enterprise/recruitment/info/recExamine'),
+        method: 'post',
+        params: this.$http.adornParams({
+          'recruitmentInfoId': this.tempRec.recruitmentInfoId,
+          'inApply': '0'
+        }, false)
+      }).then(({data}) => {
+        this.$message({
+          type: 'success',
+          message: '提交成功!'
+        })
+        this.retreatVisible = false
+        this.getDataList()
+      })
+    },
+    isDelete (item) {
+      if ((item.projectMatchApplyStatus === 0) &&
+                this.isAuth('innovate:match:delete')) {
+        return true
+      }
+    },
+        // 删除
+    deleteHandle (id) {
+      var canDelete = true
+      var matchIds = id ? [id] : this.dataListSelections.map(item => {
+        if ((item.projectInfoEntity.projectMatchApplyStatus > 0) ||
+                    !this.isAuth('innovate:match:delete')) {
+          canDelete = false
+        } else {
+          canDelete = false
+        }
+        return item.projectInfoEntity.matchId
+      })
+      this.$confirm(`确定要进行[${id ? '删除' : '批量删除'}]操作?`, '提示', {
+        confirmButtonText: '确定',
+        cancelButtonText: '取消',
+        type: 'warning'
+      }).then(() => {
+        if (canDelete) {
+          this.$http({
+            url: this.$http.adornUrl('/innovate/match/info/delete'),
+            method: 'post',
+            data: this.$http.adornData(matchIds, false)
+          }).then(({data}) => {
+            if (data && data.code === 0) {
+              this.$message({
+                message: '操作成功',
+                type: 'success',
+                duration: 1500,
+                onClose: () => {
+                  this.getDataList()
+                }
+              })
+            } else {
+              this.$message.error(data.msg)
+            }
+          })
+        } else {
+          this.$message({
+            message: '包含不可删除项目',
+            type: 'error',
+            duration: 1500,
+            onClose: () => {
+              this.getDataList()
+            }
+          })
+        }
+      }).catch(() => {
+      })
+    }
+  }
 }
 </script>
 <style>
